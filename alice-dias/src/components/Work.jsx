0ad9961b60bcd4939c9b3d6bec578510aa84ec8b
Alice Dias <8200118@estg.ipp.pt>
2025-02-14 15:28:59 +0000

    💄projects

--- alice-dias/src/components/Work.jsx
+++ alice-dias/src/components/Work.jsx
@@ -3,10 +3,14 @@
  * @license Apache-2.0
  */
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProjectCard from './ProjectCard';
 
 
-const works = [
+const API_URL = import.meta.env.VITE_API_URL;
+
+
+/* const works = [
     {
         imgSrc: '/images/project-1.jpg',
         title: 'Full stack music app',
@@ -43,12 +47,32 @@ const works = [
         tags: ['Web-design', 'Development'],
         projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
     },
-];
+]; */
 
 
 const Work = () => {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch(`${API_URL}/project`)
+            .then((res) => res.json())
+            .then((data) => setProjects(data))
+            .catch((err) => console.error("Error searching for projects:", err));
+    }, []);
+
+
     return (
-        <div>Work</div>
+        <section id='work' className="section">
+            <div className="container">
+                <h2 className="headline-2 mb-8">My portfolio highlights</h2>
+                <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
+                    {projects.map(({ id, photo, name, description, url, tecnologies }) => (
+                        <ProjectCard key={id} photo={photo} name={name} description={description} url={url} tecnologies={tecnologies.map(t => t.name)} />
+                    ))}
+                </div>
+            </div>
+        </section>
     )
 }
 
