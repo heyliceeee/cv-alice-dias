9a044de6cdc778e0eeab595f9e80c0370c6e853c
Alice Dias <8200118@estg.ipp.pt>
2025-02-14 11:32:47 +0000

    💄about

---
+++ alice-dias/src/components/About.jsx
@@ -0,0 +1,52 @@
+/**
+ * @copyright 2025 Alice Dias
+ * @license Apache-2.0
+ */
+
+import React from 'react'
+
+
+
+const aboutItems = [
+    {
+        label: 'Project done',
+        number: 80
+    },
+    {
+        label: 'Years of experience',
+        number: 7
+    }
+];
+
+const About = () => {
+    return (
+        <section id='about' className="section">
+            <div className="container">
+                <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12">
+                    <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
+                        Hi👋, I'm Alice Dias, a passionate Software Engineering degree student about programming and technology, with a solid background in Web and Mobile development.
+                    </p>
+
+                    <div className="flex flex-wrap items-center gap-4 md:gap-7">
+                        {
+                            aboutItems.map(({ label, number }, key) => (
+                                <div key={key}>
+                                    <div className="flex items-center md:mb-2">
+                                        <span className="text-2xl font-semibold md:text-4xl">{number}</span>
+                                        <span className="text-purple-400 font-semibold md:text-3xl">+</span>
+                                    </div>
+
+                                    <p className="text-sm text-zinc-400">{label}</p>
+                                </div>
+                            ))
+                        }
+
+                        <img className="ml-auto md:w-[40px] md:h-[40px]" width={30} height={30} src="/public/favicon.ico" alt="Logo" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
--- alice-dias/src/components/Hero.jsx
+++ alice-dias/src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
                         </div>
                     </div>
 
-                    <h2 className='headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10'>Building Scalable Modern Websites for the Future</h2>
+                    <h2 className='headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10'>Software Engineering Degree Student</h2>
 
                     <div className="flex items-center gap-3">
                         <PrimaryButton label="Download CV" icon="download" />
