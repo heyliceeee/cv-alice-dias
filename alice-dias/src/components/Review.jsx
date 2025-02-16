9a5dbe481eab7c8a8cf6b18776b47be4b18bfa08
Alice Dias <8200118@estg.ipp.pt>
2025-02-16 17:41:20 +0000

    💄reviews

---
+++ alice-dias/src/components/Review.jsx
@@ -0,0 +1,40 @@
+/**
+ * @copyright 2025 Alice Dias
+ * @license Apache-2.0
+ */
+
+import React from 'react'
+import ReviewCard from './ReviewCard';
+
+
+const reviews = [
+    {
+        content: 'O aluno Alice Dias, mostrou no decorrer do estágio uma enorme capacidade de trabalho tendo conseguido um resultado de excelência. Revelou conhecimentos práticos de programação, com muita facilidade percebeu a organização da empresa tendo colaborado na elaboração de uma coleção digital. O seu relacionamento com os demais professionais da Gierlings Velpor, notabilizou-se pela relação interpessoal que rapidamente conseguiu, revelando forte sentido de humildade, cooperação e interajuda sempre que lhe foi solicitado. Em conclusão direi que foi um estágio bastante produto em que as ambas as partes sairam a ganhar. Parabéns Alice!',
+        name: 'Francisco Freitas',
+        company: 'Lantal Textiles AG (Gierlings Velpor)'
+    },
+    {
+        content: 'Mentioned in the article about the Professional Aptitude Tests at "Escola Secundária de Tomaz Pelayo".',
+        name: 'Edgar Resende Borges',
+        company: 'ANPRI'
+    }
+];
+
+
+const Review = () => {
+    return (
+        <section id='reviews' className="section overflow-hidden">
+            <div className="container">
+                <h2 className="headline-2 mb-8">What people say</h2>
+
+                <div className="flex items-stretch gap-3 w-fit">
+                    {reviews.map(({ content, name, company }, key) => (
+                        <ReviewCard key={key} content={content} name={name} company={company} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Review
--- alice-dias/src/components/Skill.jsx
+++ alice-dias/src/components/Skill.jsx
@@ -97,7 +97,7 @@ const Skill = () => {
         <section className="section">
             <div className="container">
                 <h2 className="headline-2">Essential Tools I use</h2>
-                <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">Discover the powerful tools and technologies I use to create exceptional, high-performing websites & applications.</p>
+                <p className="text-zinc-400 mt-3 mb-8">Discover the powerful tools and technologies I use to create exceptional, high-performing websites & applications.</p>
                 <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
                     {
                         skillItem.map(({ imgSrc, label, desc }, key) => (
